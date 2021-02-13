7b975f6adeed5dee74cd8b57c319a36dd2d311f4
Seema Ullal <seema.ullal@gusto.com>
2021-02-12 20:07:56 -0800

    finish 06 useDebugValue

---
+++ src/exercise/06.tsx
@@ -0,0 +1,50 @@
+// useDebugValue: useMedia
+// http://localhost:3000/isolated/exercise/06.js
+
+import React, {useEffect, useState, useDebugValue} from 'react';
+
+function useMedia(query: string, initialState = false) {
+  const [state, setState] = useState(initialState);
+  useDebugValue(`\`${query}\` => ${state}`);
+  useEffect(() => {
+    let mounted = true;
+    const mql = window.matchMedia(query);
+    function onChange() {
+      if (!mounted) {
+        return;
+      }
+      setState(Boolean(mql.matches));
+    }
+
+    mql.addListener(onChange);
+    setState(mql.matches);
+
+    return () => {
+      mounted = false;
+      mql.removeListener(onChange);
+    };
+  }, [query]);
+
+  return state;
+}
+
+function Box() {
+  const isBig = useMedia('(min-width: 1000px)');
+  const isMedium = useMedia('(max-width: 999px) and (min-width: 700px)');
+  const isSmall = useMedia('(max-width: 699px)');
+  const color = isBig
+    ? 'green'
+    : isMedium
+    ? 'yellow'
+    : isSmall
+    ? 'red'
+    : undefined;
+
+  return <div style={{width: 200, height: 200, backgroundColor: color}} />;
+}
+
+function App() {
+  return <Box />;
+}
+
+export default App;
